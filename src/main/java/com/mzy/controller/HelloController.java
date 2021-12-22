fb1ca92757b048840080839423982c0d459dee97
mzy150430115 <1263112196@qq.com>
2021-12-22 11:07:51 +0800

    Initial commit

---
+++ src/main/java/com/mzy/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.mzy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author ZhiYuan Ma
+ * @create: 2021-12-22 11:06
+ * @description:
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String getHello(){
+        return "hello world";
+    }
+}
