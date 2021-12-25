c7fda9b2dc803d50d1201ad62eadc5c5a21cea93
mzy150430115 <1263112196@qq.com>
2021-12-25 11:41:17 +0800

    111

--- src/main/java/com/mzy/controller/HelloController.java
+++ src/main/java/com/mzy/controller/HelloController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String getHello(){
-        return "hello world06";
+        return "hello world07";
     }
 }
